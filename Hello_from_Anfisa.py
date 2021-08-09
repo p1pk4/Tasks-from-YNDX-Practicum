a7598225deca2a9cddc5c3946deb89292c7a8cbb
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-09 19:33:15 +0300

    Анфиса умеет здороваться, нужно добавить приветствия для ночи и вечера.

---
+++ Hello_from_Anfisa.py
@@ -0,0 +1,14 @@
+#Анфиса умеет здороваться утром и днём, но ей нужно добавить приветствия для ночи и вечера.
+#Напишите условную конструкцию, которая выводит уместные сообщения
+for current_hour in range(0, 24):
+    print("На часах " + str(current_hour) + ":00.")
+    if  current_hour < 6:
+        print('Доброй ночи!')
+    elif current_hour < 12:
+        print ('Доброе утро!')
+    elif current_hour < 18:
+        print('Добрый день!')
+    elif current_hour < 23:
+        print('Добрый вечер!')
+    else:
+        print('Доброй ночи!')
