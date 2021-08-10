9f8fe397d0d1460f5353e1353a86f934d563222f
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-10 11:07:18 +0300

    keys

--- FunctionTask2.py
+++ FunctionTask2.py
@@ -1,3 +1,4 @@
+'''
 #Напишите цикл, в котором функция print_friends_count() вызывается c аргументами от 1 до 10.
 def print_friends_count(friends_count):
     remainder = friends_count % 10
@@ -12,3 +13,13 @@ def print_friends_count(friends_count):
 
 for friends_count in range(1,11):
     print_friends_count(friends_count)
+'''
+
+def keys (key_from_home):
+    any_key = key_from_home + 20
+    if key_from_home >= 1:
+        print('на связке', key_from_home, 'ключей')
+    else:
+        print('нет ключей')
+for key_from_home in range(1, 20, 3):
+    keys(key_from_home)
